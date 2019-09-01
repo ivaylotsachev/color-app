813877baca3d856ed0456d90115ff1da7bc93c6a
itsachev <itsa4ev@gmail.com>
2019-09-01 18:03:06 +0300

    refactor styles

--- src/components/PaletteFormNav.js
+++ src/components/PaletteFormNav.js
@@ -8,46 +8,10 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Button } from '@material-ui/core';
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { ValidatorForm } from 'react-material-ui-form-validator';
 import { withStyles } from '@material-ui/core/styles';
 import PaletteMetaForm from './PaletteMetaForm';
-
-const drawerWidth = 400;
-
-const styles = theme => ({
-    root: {
-        display: 'flex'
-    },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        }),
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen
-        })
-    },
-    menuButton: {
-        marginRight: theme.spacing(2)
-    },
-    navBtns: {
-        marginRight: '1rem'
-    },
-    button: {
-        margin: '0 0.5rem'
-    },
-    link: {
-        textDecoration: 'none'
-    }
-});
+import styles from '../styles/PaletteFormNavStyles';
 
 class PaletteFormNav extends Component {
     constructor(props) {
@@ -57,18 +21,16 @@ class PaletteFormNav extends Component {
             newPaletteName: '',
             formShowing: false
         };
-
-        console.error('paletteformnac', this.props);
     }
 
-    componentDidMount() {
-        ValidatorForm.addValidationRule('isPaletteNameUnique', value =>
-            this.props.palettes.every(
-                ({ paletteName }) =>
-                    paletteName.toLowerCase() !== value.toLowerCase()
-            )
-        );
-    }
+    // componentDidMount() {
+    //     ValidatorForm.addValidationRule('isPaletteNameUnique', value =>
+    //         this.props.palettes.every(
+    //             ({ paletteName }) =>
+    //                 paletteName.toLowerCase() !== value.toLowerCase()
+    //         )
+    //     );
+    // }
 
     handleChange = event => {
         this.setState({
