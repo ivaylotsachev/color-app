e5ab80d0b83fd8cac71f0670106496d288326a65
itsachev <itsa4ev@gmail.com>
2019-09-01 21:04:41 +0300

    Updates

---
+++ precache-manifest.1bbc6bdbf821a93d7b2d055f1c94fb87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51135a283fa43e0bd3b324c5fb9f15d4",
+    "url": "/ivaylotsachev/color-app/index.html"
+  },
+  {
+    "revision": "56a1ee3491daefaab94f",
+    "url": "/ivaylotsachev/color-app/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "a2591d99dce340c316cd",
+    "url": "/ivaylotsachev/color-app/static/css/main.d9c864f2.chunk.css"
+  },
+  {
+    "revision": "56a1ee3491daefaab94f",
+    "url": "/ivaylotsachev/color-app/static/js/2.5a3ec575.chunk.js"
+  },
+  {
+    "revision": "a2591d99dce340c316cd",
+    "url": "/ivaylotsachev/color-app/static/js/main.b639b65c.chunk.js"
+  },
+  {
+    "revision": "3bf484fdc6eea9a48f16",
+    "url": "/ivaylotsachev/color-app/static/js/runtime~main.9adcf972.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/ivaylotsachev/color-app/static/media/bg.77428cd4.svg"
+  }
+]);
