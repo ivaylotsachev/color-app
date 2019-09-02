e3ffcf9813e04e684fd8814a2c15315d6d4d0fb8
itsachev <itsa4ev@gmail.com>
2019-09-02 17:53:48 +0300

    Updates

--- precache-manifest.5b46a263dbb459ab1e16140ff6fed502.js
+++ precache-manifest.5b46a263dbb459ab1e16140ff6fed502.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9ef62343e4d1d5d732a66d92680b29bd",
+    "revision": "da08d783e8262aec044b975d8cea17f7",
     "url": "/color-app/index.html"
   },
   {
-    "revision": "36f7cc9da89ff07b6f46",
+    "revision": "ec7af86b44bed1fb804b",
     "url": "/color-app/static/css/2.e8e75e1a.chunk.css"
   },
   {
-    "revision": "27dded911ca722cfa4ee",
-    "url": "/color-app/static/css/main.d9c864f2.chunk.css"
+    "revision": "1f6af9f8e01425d359c5",
+    "url": "/color-app/static/css/main.18d7283d.chunk.css"
   },
   {
-    "revision": "36f7cc9da89ff07b6f46",
-    "url": "/color-app/static/js/2.0e5a8396.chunk.js"
+    "revision": "ec7af86b44bed1fb804b",
+    "url": "/color-app/static/js/2.c5a1f2d0.chunk.js"
   },
   {
-    "revision": "27dded911ca722cfa4ee",
-    "url": "/color-app/static/js/main.bd899bb7.chunk.js"
+    "revision": "1f6af9f8e01425d359c5",
+    "url": "/color-app/static/js/main.8a41faa1.chunk.js"
   },
   {
     "revision": "ec97bb2069fdb06974f1",
